e4ae21e8e8e503cdc3cca55646b5d438f7e8aa58
spyboost <spyboost@gmail.com>
2012-08-07 20:11:28 -0400

    max sub sum.

--- src/collection/collection.js
+++ src/collection/collection.js
@@ -3,53 +3,64 @@
 Array.prototype.maxSubSum =
 /**
  * Finds the max subsequence sum.
- * @param {Boolean} [returnSubsequence = false] the flag indicating that the subsequence is returned instead of just sum.
+ * @param {Boolean} [returnRange = false] the flag indicating that the subsequence is returned instead of just sum.
  * @returns the max subsequence sum
  */
-function maxSubSum(returnSubsequence){
+function maxSubSum(returnRange){
 	if(this.length <= 1)
 		return this[0];
-	var maxSum = undefined;
-	var sum = 0;
+	var maxMin = -Infinity;
 	var start = 0;
-	var end;
-	var maxStart;
-	var maxEnd;
-	var negativeSum = 0;
-	for(var i = 0; i < this.length; i++){
+	var end = 0;
+	for(var i = 0; i < this.length && this[i] <= 0; i++)
+		if(this[i] > maxMin){
+			start = i;
+			end = i;
+			maxMin = this[i];
+		}
+	if(i == this.length){
+		if(returnRange)
+			return [start, end];
+
+		return maxMin;
+	}
+	if(i > 0)
+		i--;
+	var sum = this[i];
+	start = i;
+	end = i;
+	var maxSum = this[i];
+	var maxStart = i;
+	var maxEnd = i;
+	for(++i; i < this.length; i++){
 		var v = this[i];
 		if(v > 0){
-			end = i;
-			if(negativeSum){
-				var currentSum = sum + negativeSum;
-				if(currentSum > 0)
-					sum = currentSum + v;
-				else if(maxSum == undefined || sum > maxSum){
-					maxStart = start;
-					maxEnd = end;
-					maxSum = sum;
-					start = end;
-				}
-				negativeSum = 0;
-			}else{
+			if(sum <= 0){
+				sum = v;
+				start = i;
+			}else
 				sum += v;
-				if(maxSum < 0){
-					maxSum = v;
-					start = v;
-					end = v;
-				}
+			end = i;
+		}else{
+			if(sum > maxSum){
+				maxSum = sum;
+				maxStart = start;
+				maxEnd = end;
 			}
-		}else if(sum > 0)
-			negativeSum += v;
-		else if(maxSum == undefined || v > maxSum){
-			maxSum = v;
-			maxStart = i;
-			maxEnd = i;
+			for(; i < this.length && this[i] <= 0; i++)
+				if(sum > 0)
+					sum += this[i];
+			if(i < this.length)
+				i--;
 		}
 	}
-	if(maxSum < 0)
-		return maxSum;
-	return sum;
-
+	if(sum > maxSum){
+		maxSum = sum;
+		maxStart = start;
+		maxEnd = end;
+	}
+	if(returnRange)
+		return [maxStart, maxEnd];
+	return maxSum;
 };
 })();
