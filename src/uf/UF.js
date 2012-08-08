b6870d3ec9bff308e344f84f1ffec3ed0d210142
spyboost <spyboost@gmail.com>
2012-08-07 21:08:35 -0400

    docs.

--- src/uf/UF.js
+++ src/uf/UF.js
@@ -1,8 +1,41 @@
 'use strict';
 window.bigo = window.bigo || {};
-bigo.UF = /**
- * @param {Number} size
- * @returns a new instance of {@link bigo.UF}
+bigo.UF =
+/**
+ * The weighted quick-union solution to dynamic connectivity problem. For the given input numbers it finds whether a given pairs of numbers are connected.
+ * The connection of two numbers is a equivalence relation, which means:
+ * <ul>
+ * <li><b>Reflexive</b>: p is connected to p.</li>
+ * <li><b>Symmetric</b>: if p is connected to q, then q is connected to p.</li>
+ * <li><b>Transitive</b>: if p is connected to q and q is connected to r, then p is connected to r.</li>
+ * </ul>
+ * @example
+ * <code>
+ * <pre>
+ * var uf = new bigo.UF(1000);
+ * uf.union(1,2);
+ * uf.union(2,3);
+ * uf.union(4,5);
+ * uf.union(6,7);
+ * uf.union(4,7);
+ * uf.union(8,9);
+ * uf.union(10,11);
+ * assertTrue(uf.connected(1,1)); // reflexive: 1-1
+ * assertTrue(uf.connected(1,2));
+ * assertTrue(uf.connected(2,1)); // symmetric: 1-2 => 2-1
+ *
+ * assertTrue(uf.connected(2,3));
+ * assertTrue(uf.connected(3,1)); // transitive: 1-2 and 2-3 => 1-3
+ * assertTrue(uf.connected(4,5));
+ * assertTrue(uf.connected(5,6));
+ * assertTrue(uf.connected(6,7));
+ * assertTrue(uf.connected(5,7));
+ * assertFalse(uf.connected(7,8));
+ * assertFalse(uf.connected(9,10));
+ * </pre>
+ * </code>
+ * @param {Number} size - the number of elements
+ * @returns {bigo.UF} a new instance of {@link bigo.UF}
  */
 function UF(size){
 	var count = size;
