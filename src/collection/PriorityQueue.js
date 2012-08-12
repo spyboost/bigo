90bcb91a3bc4ac37142f315e778c3ceb1932c692
spyboost <spyboost@gmail.com>
2012-08-12 10:02:42 -0400

    priority queue enhancement.

--- src/collection/PriorityQueue.js
+++ src/collection/PriorityQueue.js
@@ -15,7 +15,7 @@ bigo.collection = bigo.collection || {};
  * <li>O(N) - for the <code>remove(element)</code> method</li>
  * <li>constant time - for the retrieval methods(<code>peek()</code>, <code>size()</code>) and the <code>length</code> and <code>size</code> properties</li>
  * </ul>
- * @param comparator Function the comparator used to compare elements in the queue
+ * @param {Function} [comparator] the comparator used to compare elements in the queue
  * @constructor
  */
 bigo.collection.PriorityQueue = function(comparator){
@@ -27,30 +27,60 @@ bigo.collection.PriorityQueue = function(comparator){
 	 * @param element the element to insert
 	 */
 	this.offer = function(element){
-		var s = size;
+		var position = size;
 		size++;
-		if(s)
-			siftUp(s, element);
+		if(position)
+			siftUp(position, element);
 		else
 			q[0] = element;
 	};
 
-	function siftUp(s, element){
+	function siftUp(position, element){
+		if (comparator)
+			siftUpUsingComparator(position, element);
+        else
+            siftUpElement(position, element);
+	}
+
+	function siftUpElement(position, element){
+		var k = position;
+		if(element.compareTo)
+			while (k > 0) {
+	            var parentIndex = (k - 1) >>> 1;
+	            var parent = q[parentIndex];
+	            if (element.compareTo(parent) >= 0)
+	                break;
+	            q[k] = parent;
+	            k = parentIndex;
+	        }
+		else
+			while (k > 0) {
+	            var parentIndex = (k - 1) >>> 1;
+	            var parent = q[parentIndex];
+	            if (element >= parent)
+	                break;
+	            q[k] = parent;
+	            k = parentIndex;
+	        }
+        q[k] = element;
+	}
+
+	function siftUpUsingComparator(s, element){
 		var k = s;
 		while(k > 0) {
-            var parent = (k - 1) >>> 1;
-            var e = q[parent];
+            var parentIndex = (k - 1) >>> 1;
+            var e = q[parentIndex];
             if (comparator(element, e) >= 0)
                 break;
             q[k] = e;
-            k = parent;
+            k = parentIndex;
         }
         q[k] = element;
 	}
 
 	/**
 	 * Retrieves but doesn't remove the head of the queue.
-	 * @return the head of the queue, or <code>null</code> if the queue is empty
+	 * @return the head of the queue, or <code>undefined</code> if the queue is empty
 	 */
 	this.peek = function(){
 		if (size)
@@ -59,14 +89,14 @@ bigo.collection.PriorityQueue = function(comparator){
 
 	/**
 	 * Retrieves and removes the head of the queue.
-	 * @return the head of the queue, or <code>null</code> if the queue is empty
+	 * @return the head of the queue, or <code>undefined</code> if the queue is empty
 	 */
 	this.poll = function(){
 		if(size){
 	        var s = --size;
 	        var result = q[0];
 	        var element = q[s];
-	        q[s] = null;
+	        q[s] = undefined;
 	        if(s)
 	            siftDown(0, element);
 	        return result;
@@ -74,6 +104,43 @@ bigo.collection.PriorityQueue = function(comparator){
 	};
 
 	function siftDown(i, element){
+		if (comparator)
+            siftDownUsingComparator(i, element);
+        else
+            siftDownElement(i, element);
+	}
+
+	function siftDownElement(i, element){
+		var k = i;
+		var half = size >>> 1;
+		if(element.compareTo)
+	        while (k < half) {
+	            var child = (k << 1) + 1;
+	            var c = q[child];
+	            var right = child + 1;
+	            if (right < size && c.compareTo(q[right]) > 0)
+	                c = q[child = right];
+	            if (element.compareTo(c) <= 0)
+	                break;
+	            q[k] = c;
+	            k = child;
+	        }
+		else
+			while (k < half) {
+	            var child = (k << 1) + 1;
+	            var c = q[child];
+	            var right = child + 1;
+	            if (right < size && c > q[right])
+	                c = q[child = right];
+	            if (element <= c)
+	                break;
+	            q[k] = c;
+	            k = child;
+	        }
+        q[k] = element;
+	}
+
+	function siftDownUsingComparator(i, element){
 		var k = i;
 		var half = size >>> 1;
         while (k < half) {
@@ -107,10 +174,10 @@ bigo.collection.PriorityQueue = function(comparator){
 	function removeAt(i){
 		var s = --size;
         if (s == i)
-            q[i] = null;
+            q[i] = undefined;
         else {
             var moved = q[s];
-            q[s] = null;
+            q[s] = undefined;
             siftDown(i, moved);
             if (q[i] == moved) {
                 siftUp(i, moved);
@@ -118,7 +185,6 @@ bigo.collection.PriorityQueue = function(comparator){
                     return moved;
             }
         }
-        return null;
 	}
 
 	Object.defineProperty(this, 'length', {
--- test/collection/PriorityQueueTest.js
+++ test/collection/PriorityQueueTest.js
@@ -1,63 +1,103 @@
 'use strict';
 var PriorityQueueTest = TestCase('PriorityQueue');
 PriorityQueueTest.prototype.testPriorityQueue = function(){
-	var pq = new bigo.collection.PriorityQueue(function(a, b){
-		return a < b ? -1 : a == b ? 0 : 1;
+	var pqs = [
+       ['comparator', new bigo.collection.PriorityQueue(function(a, b){
+    	   	return a < b ? -1 : a == b ? 0 : 1;	})],
+	   ['no comparator', new bigo.collection.PriorityQueue()]
+    ];
+	pqs.forEach(function(testUnit){
+		var info = testUnit[0];
+		var pq = testUnit[1];
+		assertEquals([info, ':', 'initial length'].join(''), 0, pq.length);
+		assertEquals([info, ':', 'initial size'].join(''), 0, pq.size);
+		pq.offer(1);
+		assertEquals([info, ':', 'length'].join(''), 1, pq.length);
+		assertEquals([info, ':', 'size'].join(''), 1, pq.size);
+		assertEquals([info, ':'].join(''), 1, pq.peek());
+		assertEquals([info, ':', 'length'].join(''), 1, pq.length);
+		assertEquals([info, ':', 'size'].join(''), 1, pq.size);
+		pq.offer(2);
+		assertEquals([info, ':','length'].join(''), 2, pq.length);
+		assertEquals([info, ':','size'].join(''), 2, pq.size);
+		assertEquals([info, ':'].join(''),1, pq.peek());
+		assertEquals([info, ':','length'].join(''), 2, pq.length);
+		assertEquals([info, ':','size'].join(''), 2, pq.size);
+		pq.offer(0);
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 3, pq.size);
+		assertEquals([info, ':'].join(''), 0, pq.peek());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 3, pq.size);
+
+		assertEquals([info, ':'].join(''), 0, pq.poll());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 2, pq.size);
+
+		assertEquals([info, ':'].join(''), 1, pq.poll());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 1, pq.size);
+
+		assertEquals([info, ':'].join(''), 2, pq.poll());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 0, pq.size);
+
+		assertUndefined([info, ':'].join(''), pq.poll());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 0, pq.size);
+
+		assertUndefined([info, ':'].join(''), pq.peek());
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 0, pq.size);
+
+		pq.offer(3);
+		pq.offer(0);
+		pq.offer(7);
+		assertEquals([info, ':','length'].join(''), 3, pq.length);
+		assertEquals([info, ':','size'].join(''), 3, pq.size);
+		pq.offer(2);
+		assertEquals([info, ':','length'].join(''), 4, pq.length);
+		assertEquals([info, ':','size'].join(''), 4, pq.size);
+		assertTrue(pq.remove(7));
+		assertEquals([info, ':','length'].join(''), 4, pq.length);
+		assertEquals([info, ':','size'].join(''), 3, pq.size);
+		assertFalse(pq.remove(7));
+		assertEquals([info, ':','length'].join(''), 4, pq.length);
+		assertEquals([info, ':','size'].join(''), 3, pq.size);
 	});
-
-	assertEquals('initial length', 0, pq.length);
-	assertEquals('initial size', 0, pq.size);
-	pq.offer(1);
-	assertEquals('length', 1, pq.length);
-	assertEquals('size', 1, pq.size);
-	assertEquals(1, pq.peek());
-	assertEquals('length', 1, pq.length);
-	assertEquals('size', 1, pq.size);
-	pq.offer(2);
-	assertEquals('length', 2, pq.length);
-	assertEquals('size', 2, pq.size);
-	assertEquals(1, pq.peek());
-	assertEquals('length', 2, pq.length);
-	assertEquals('size', 2, pq.size);
-	pq.offer(0);
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 3, pq.size);
-	assertEquals(0, pq.peek());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 3, pq.size);
-
-	assertEquals(0, pq.poll());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 2, pq.size);
-
-	assertEquals(1, pq.poll());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 1, pq.size);
-
-	assertEquals(2, pq.poll());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 0, pq.size);
-
-	assertEquals(null, pq.poll());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 0, pq.size);
-
-	assertEquals(null, pq.peek());
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 0, pq.size);
-
-	pq.offer(3);
-	pq.offer(0);
-	pq.offer(7);
-	assertEquals('length', 3, pq.length);
-	assertEquals('size', 3, pq.size);
-	pq.offer(2);
-	assertEquals('length', 4, pq.length);
-	assertEquals('size', 4, pq.size);
-	assertTrue(pq.remove(7));
-	assertEquals('length', 4, pq.length);
-	assertEquals('size', 3, pq.size);
-	assertFalse(pq.remove(7));
-	assertEquals('length', 4, pq.length);
-	assertEquals('size', 3, pq.size);
+};
+PriorityQueueTest.prototype.testPriorityQueueUsingComareTo = function(){
+	var pq = new bigo.collection.PriorityQueue();
+	var compareTo = function(other){
+		return this.data < other.data ? -1 : this.data == other.data ? 0 : 1;
+	};
+	var load = [
+        {data: 'c'},
+        {data: 'a'},
+        {data: 'b'},
+        {data: 'z'},
+        {data: 'y'},
+    ].map(function(it){
+    	it.compareTo = compareTo;
+    	return it;
+    });
+	load.forEach(function(it){
+		pq.offer(it);
+	});
+	assertEquals(['compareTo', ': ', 'size'].join(''), load.length, pq.size);
+	assertEquals(['compareTo', ': ', 'length'].join(''), load.length, pq.length);
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[1], pq.peek());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[1], pq.poll());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[2], pq.peek());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[2], pq.poll());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[0], pq.peek());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[0], pq.poll());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[4], pq.peek());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[4], pq.poll());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[3], pq.peek());
+	assertSame(['compareTo', ': ', 'peek'].join(''), load[3], pq.poll());
+	assertUndefined(['compareTo', ': ', 'peek'].join(''), pq.peek());
+	assertUndefined(['compareTo', ': ', 'peek'].join(''), pq.poll());
+	assertEquals(['compareTo', ': ', 'size'].join(''), 0, pq.size);
+	assertEquals(['compareTo', ': ', 'length'].join(''), load.length, pq.length);
 };
